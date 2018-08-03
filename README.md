b3cda2c42f0e475d7ef64bf20c5ea7b3b454b654
Kyusun Shim <kyusunangel@gmail.com>
2018-08-03 13:18:18 +0900

    Fixed Error and handled exceptions

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 Connect to the virus total and send files(include Compressed files after decompress)
 
 You need import some python packages
-- pandas, patool
+- pandas, patool, requests
 
 If you want decompress RAR files, should be install RAR decompress program in your system
 - I checked WinRAR
+
+Using API, it take long time analyzing new file(not in VirusTotal Database)
+
+So you should be rescan after long time(several hours or several days)
--- chVT.py
+++ chVT.py
@@ -5,15 +5,20 @@
 # Rewritten & Modified: Chris Clark
 # ------------------------
 
-
+from __future__ import division
 import json, urllib, urllib2, hashlib, re
 from pprint import pprint
-import csv
 import zipfile
 import os
 import patoolib
 import pandas as pd
 from time import sleep
+import requests
+import sys
+import string
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 commpress_file_ext_list = ['.zip', '.rar', '.tar', '.egg']
 
@@ -28,12 +33,13 @@ class UNZip():
     def decompressFile(self, path_name):
         for file in self.search(path_name):
             target_path = path_name + file
-            #    print target_path
             if os.path.isdir(target_path):
                 self.decompressFile(target_path + "\\")
             else:
                 fname, ext = os.path.splitext(target_path)
-                self.decompress_file_list.append(target_path)
+                # If file is binary, add to the list
+                if self.isbinaryfile(target_path):
+                    self.decompress_file_list.append(target_path)
 
                 if ext in commpress_file_ext_list:
                     dir_name = path_name + os.path.splitext(file)[0] + "_unzip"
@@ -58,6 +64,36 @@ class UNZip():
         filename_list = os.listdir(path_name)
         return filename_list
 
+    def allSearch(self, path_name):
+        filenames = os.listdir(path_name)
+        for filename in filenames:
+            full_filename = os.path.join(path_name, filename)
+            if os.path.isdir(full_filename):
+                self.allSearch(full_filename)
+            else:
+                # If file is binary, add to the list
+                if self.isbinaryfile(full_filename):
+                    self.decompress_file_list.append(full_filename)
+
+    def isbinaryfile(self, filename):
+        s = open(filename, 'r').read(512)
+        text_characters = "".join(map(chr, range(32,127)) + list("\n\r\t\b"))
+        _null_trans = string.maketrans("","")
+        if not s:
+            #Empty files are considered text
+            return False
+        if "\0" in s:
+            #Files with null bytes are likely binary
+            return True
+        # Get the non-text characters (map a character to itself then
+        # use the 'remove' option to get rid of the text characters.)
+        t = s.translate(_null_trans, text_characters)
+        # If more than 30% non-text characters, then
+        # this is considered a binary file
+        if float(len(t))/float(len(s)) > 0.30:
+            return True
+        return False
+
 
 class vtAPI():
 
@@ -68,24 +104,43 @@ class vtAPI():
         self.api = f.readline()
 
     def getReport(self, md5):
-        param = {'resource': md5, 'apikey': self.api, 'allinfo': '1'}
-        url = self.base + "file/report"
-        data = urllib.urlencode(param)
-        result = urllib2.urlopen(url, data)
-        jdata = json.loads(result.read())
-        print jdata
-        return jdata
-
-    def rescan(self, md5):
-        param = {'resource': md5, 'apikey': self.api}
-        url = self.base + "file/rescan"
-        data = urllib.urlencode(param)
-        result = urllib2.urlopen(url, data)
-        print "\n\tVirus Total Rescan Initiated for -- " + md5 + " (Requery in 10 Mins)"
-        jdata = json.loads(result.read())
-        return jdata
+        try:
+            param = {'resource': md5, 'apikey': self.api, 'allinfo': '1'}
+            url = self.base + "file/report"
+            data = urllib.urlencode(param)
+            result = urllib2.urlopen(url, data)
+            jdata = json.loads(result.read())
+            #print jdata
+            return jdata
+        except:
+            e = sys.exc_info()[0]
+            print e
+            return 0
 
 
+    def scan(self, _files):
+        try:
+            param = {'apikey': self.api}
+            files = {'file':(_files, open(_files), 'rb')}
+            response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params = param)
+            result = response.json()
+        except:
+            e = sys.exc_info()[0]
+            print e
+            pass
+
+    def rescan(self, _md5):
+        try:
+            param = {'resource':_md5, 'apikey': self.api}
+            url = self.base + "file/rescan"
+            data = urllib.urlencode(param)
+            result = urllib2.urlopen(url, data)
+            jdata = json.loads(result.read())
+            return jdata['response_code']
+        except:
+            e = sys.exc_info()[0]
+            print e
+            return -1
 # Md5 Function
 
 def checkMD5(checkval):
@@ -113,20 +168,22 @@ def parse(it, md5, file_name):
     Result_rows = list()
     Version_rows = list()
     update_rows = list()
+    if it == 0:
+        print "JSON is null"
+        return 0
+    if int(it['response_code']) == 0:
+        print md5 + " -- Not Found in VT"
+        return 0
+    elif int(it['response_code']) == -2:
+        print md5 + " -- Queuing in VT"
+        return -2
+    elif int(it['positives']) > 0:
+        print "Checked Virus File: ", it['positives'],  ' / ', it['total']
+        virus_result = it['positives'],  ' / ', it['total']
+        rows = [file_name, md5, it['permalink'], virus_result]
+        pd_frame = pd.DataFrame(rows)
+        pd_frame.to_csv("result.csv", mode='a')
 
-    if it['positives'] > 0:
-        if it['response_code'] == 0:
-            print md5 + " -- Not Found in VT"
-            return 0
-
-        print '\tScanned on:', it['scan_date']
-        print '\n\tResult Link:', it['permalink']
-       # print '\n\tVerbose VirusTotal Information Output:\n'
-
-      #  print "\n\tResults for MD5: ", it['md5'], "\n\n\tDetected by: ", it['positives'], '/', it['total'], '\n'
-
-        #print '\n\tAntiVirus\tDetected\tResult\tVersion\tUpdate date'
-        # writeCSV(["AntiVirus", "Detected", "Result", "Version", "Update date"])
         for x in it['scans']:
             #   print '\t', x, '\t' if len(x) < 7 else '', '\t' if len(x) < 14 else '', '\t', it['scans'][x][
             #       'detected'], '\t', it['scans'][x]['result'], '\t', it['scans'][x]['version'], '\t', it['scans'][x]['update']
@@ -136,9 +193,6 @@ def parse(it, md5, file_name):
             Version_rows.append(it['scans'][x]['version'])
             update_rows.append(it['scans'][x]['update'])
 
-        rows = [file_name, md5, it['permalink']]
-        pd_frame = pd.DataFrame(rows)
-        pd_frame.to_csv("result.csv", mode='a')
 
         dic = {'AntiVirus': antiVirus_rows,
                'Detected': detected_rows,
@@ -147,37 +201,72 @@ def parse(it, md5, file_name):
                'Version': Version_rows}
         frame = pd.DataFrame(dic)
         frame.to_csv("result.csv", mode="a")
+        return 1
+    else:
+        print "Not Virus File"
 
-
-def reUnzip(path):
+def GetFileList(path, is_compressed):
     uzip = UNZip()
-    uzip.decompressFile(path)
+    if is_compressed:
+        uzip.decompressFile(path)
+    else:
+        uzip.allSearch(path)
     file_list = uzip.getDecompressFileList()
     return file_list
 
 
 def main():
-    file_list = reUnzip("C:\\Users\\NFS\\Desktop\\test\\test2\\test4\\")
+    file_list = GetFileList("C:\\Users\\test32\\Desktop\\test\\", False)
     md5_list = list()
-    cnt = 0;
+    queing_list = list()
+    cnt = 0
+    check_count = 0
+    tot_cnt = len(file_list)
+    vt = vtAPI("publicKey.txt")
     for file_name in file_list:
-        #   HashorPath = "myfile.dll"
+        file_info = os.stat(file_name)
+        check_count = check_count + 1
+        print "Processed : ", check_count, "/", tot_cnt
+        if file_info.st_size < 31457280:
+            md5 = checkMD5(file_name)
+            print file_name, md5
+            if md5 not in md5_list:
+                md5_list.append(md5)
+                ret_code = vt.rescan(md5)
+                if int(ret_code) == 0:
+                    vt.scan(file_name)
+                    queing_list.append([md5, file_name])
+                    print "Recheck Again"
+                elif int(ret_code) == -1:
+                    print "Exception Error"
+                else:
+                    parse(vt.getReport(md5), md5, file_name)
+                cnt = cnt + 1
+                if cnt % 2 == 0:
+                    sleep(60)
+        else:
+            print file_name + " : Size is greater than 30MB(Limit VT API)"
 
-        md5 = checkMD5(file_name)
-        print file_name, md5
-        if md5 not in md5_list:
-            md5_list.append(md5)
 
-           # vt = vtAPI("publicKey.txt")
-           # vt.rescan(md5)
-           # parse(vt.getReport(md5), md5, file_name)
-            cnt = cnt + 1;
-            print cnt
-           # if cnt % 2 == 0:
-           #     sleep(60)
 
-    print len(file_list)
-    print len(md5_list)
+    for recheck_md5, file_name in queing_list:
+        vt.rescan(recheck_md5)
+        parse(vt.getReport(recheck_md5), recheck_md5, file_name)
+    print "Total Checked Files : ", len(file_list)
+    print "Total Sending Files : ",len(md5_list)
+    print "Total ReChecked Files : ", len(queing_list)
+
+#Module Test
+    # file_name = "Cases.dll"
+    # md5 = checkMD5(file_name)
+    # vt=vtAPI("publicKey.txt")
+    # vt.scan(file_name)
+    # parse(vt.getReport(md5), md5, file_name)
+
+     # vt=vtAPI("publicKey.txt")
+     # vt.rescan("20F36363556DDD8FB22903E5FCB922F0")
+     # parse(vt.getReport("20F36363556DDD8FB22903E5FCB922F0"), "20F36363556DDD8FB22903E5FCB922F0", "AAAA")
+
 
 
 if __name__ == '__main__':
